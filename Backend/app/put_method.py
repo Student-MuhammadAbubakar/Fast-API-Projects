9347c81bee1b98357f3926fb6234a11af56b51a4
Student-MuhammadAbubakar <muhammadabubakar76969@gmail.com>
2026-02-25 06:18:55 -0800

    Methods Practice

---
+++ Backend/app/put_method.py
@@ -0,0 +1,65 @@
+from fastapi import FastAPI, HTTPException, status
+from typing import Any
+from scalar_fastapi import get_scalar_api_reference
+
+app = FastAPI()
+shipments = {
+    2021: {
+        "Contact no": "030803",
+        "Name ": "MUHAMMAD ABUBAKAR",
+        "Address": "Faisalabad",
+    },
+    2022: {
+        "Contact no": "03123456789",
+        "Name ": "ALI KHAN",
+        "Address": "Lahore",
+    },
+    2023: {
+        "Contact no": "03219876543",
+        "Name ": "SARA BIBI",
+        "Address": "Karachi",
+    },
+    2024: {
+        "Contact no": "03330001122",
+        "Name ": "OMAR RAHMAN",
+        "Address": "Islamabad",
+    },
+    2025: {
+        "Contact no": "03441234567",
+        "Name ": "AYESHA NOOR",
+        "Address": "Peshawar",
+    },
+}
+
+
+@app.get("/shipment")
+def GetShipment(id: int) -> dict[str, Any]:
+    if id not in shipments:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Id is not found"
+        )
+
+    return shipments[id]
+
+
+@app.post("/shipment")
+def submit_shipment(Contact_no: str, Name: str, address: str) -> dict[str, str]:
+    new_id = max(shipments.keys()) + 1
+    shipments[new_id] = {"Contact no": Contact_no, "Name": Name, "Address": address}
+    return {"Id": str(new_id)}
+
+
+@app.put("/shipment")
+def update_shipment(
+    id: int, Contact_No: str, Name: str, Address: str
+) -> dict[str, Any]:
+    shipments[id] = {"Contact no": Contact_No, "Name": Name, "Address": Address}
+    return shipments[id]
+
+
+@app.get("/scalar")
+def get_Scalar_docs():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="scalar Api",
+    )
